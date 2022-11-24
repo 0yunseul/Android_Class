52349af927a678c49b4d67a29823f16161acf22a
jooyoung <109652513+0yunseul@users.noreply.github.com>
2022-11-25 00:58:38 +0900

    edited

--- Project_jy/app/src/main/java/com/example/project_jy/MainActivity.java
+++ Project_jy/app/src/main/java/com/example/project_jy/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.project_jy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
