7a40d10fe91d7d8c0517923d0c1c33882ce44804
jooyoung <jooyoung.cairo@gmail.com>
2022-12-09 09:13:01 +0900

    1208

--- Project01_Clone/app/src/main/java/com/example/project01_clone/external/ExternalFragment.java
+++ Project01_Clone/app/src/main/java/com/example/project01_clone/external/ExternalFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.bumptech.glide.Glide;
+
 import com.example.project01_clone.R;
 
 public class ExternalFragment extends Fragment {
@@ -23,11 +23,11 @@ public class ExternalFragment extends Fragment {
         imgv1  = v.findViewById(R.id.imgv_1);
         imgv2 = v.findViewById(R.id.imgv_2);
 
-        Glide.with(this).load(
-                "https://img.freepik.com/free-photo/composition-with-christmas-decorations-in-the-interior_169016-5544.jpg?w=2000").into(imgv1);
+       // Glide.with(this).load(
+                //"https://img.freepik.com/free-photo/composition-with-christmas-decorations-in-the-interior_169016-5544.jpg?w=2000").into(imgv1);
 
-        Glide.with(this).load(
-                "https://i.pinimg.com/originals/c0/aa/5e/c0aa5e597604a5b07c3b9761d4ddd431.gif").into(imgv2);
+       // Glide.with(this).load(
+                //"https://i.pinimg.com/originals/c0/aa/5e/c0aa5e597604a5b07c3b9761d4ddd431.gif").into(imgv2);
 
         return v;
     }
--- Project_Clone/app/src/main/java/com/example/project_clone/MainActivity.java
+++ Project_Clone/app/src/main/java/com/example/project_clone/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.project_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void getAppKeyHash() {
+        try { PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),
+                PackageManager.GET_SIGNATURES); for (Signature signature : info.signatures)
+                { MessageDigest md; md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    String something = new String(Base64.encode(md.digest(), 0)); Log.d("yyg", "key: " + something); } } catch (Exception e) {
+        // TODO Auto-generated catch block Log.e("name not found", e.toString());
+        }
+    }
+
+
 }
